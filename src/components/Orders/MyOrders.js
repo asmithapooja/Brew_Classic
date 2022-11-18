a0a4202eaeb07daa324b8aa91a086c361ae6c9ff
Salman_JS <salman4javascript@gmail.com>
2022-11-19 01:47:53 +0530

    Token authentication implemented

--- src/components/Orders/MyOrders.js
+++ src/components/Orders/MyOrders.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import Footer from '../Footer/Footer';
 import Loading from '../LoadingScreen/Loading';
-import React, {useState, useEffect } from 'react';
+import changeScreen from '../Action';
+import React, { useState, useEffect } from 'react';
 import Variables from '../Variables';
+import CustomError from '../CustomError';
 import Order from './src/Order';
 import { Link, useParams } from "react-router-dom";
 import Navbar from '../Navbar/Navbar';
@@ -13,6 +15,9 @@ const MyOrders = () => {
      const { id } = useParams();
      const splitedIds = id.split(/[-]/);
 
+     // Authentication token
+     const token = localStorage.getItem("token");
+
      // Loader
      const [loading, setLoading] = useState(false);
 
@@ -22,7 +27,7 @@ const MyOrders = () => {
      const getData = () => {
           setLoading(true);
           const credentials = {
-               roomId : splitedIds[1]
+               roomId: splitedIds[1]
           }
           axios.post(`${Variables.host}/${splitedIds[1]}/userdishes`, credentials)
                .then(res => {
@@ -34,6 +39,35 @@ const MyOrders = () => {
                })
      };
 
+     // Checking the expire time of the token...
+  const parseJwt = (token) => {
+     try {
+       return JSON.parse(atob(token.split(".")[1]));
+     } catch (e) {
+       return null;
+     }
+   };
+ 
+   const AuthVerify = () => {
+     const user = localStorage.getItem("token");
+ 
+     if (user) {
+       const decodedJwt = parseJwt(user);
+ 
+       if (decodedJwt.exp * 1000 < Date.now()) {
+         localStorage.clear();
+         changeScreen(id);
+       }
+     }
+   }
+ 
+   useEffect(() => {
+     const interval = setInterval(() => {
+       AuthVerify();
+     }, 9000)
+     return () => clearInterval(interval)
+   }, [])
+
      useEffect(() => {
           getData();
      }, [])
@@ -43,23 +77,27 @@ const MyOrders = () => {
                <Navbar id={id} lodgeId={splitedIds[0]} roomId={splitedIds[1]} />
                {
                     loading ? (
-                         <Loading alignOperator = {"top-align-nav-present"} />
+                         <Loading alignOperator={"top-align-nav-present"} />
                     ) : (
-                         <div>
-                              <div className="topic text-center">
-                    My Orders!!!
-               </div>
-               <div className = "myOrders-Space">
-                    {
-                         data.map((item,key) => {
-                              return(
-                                   <Order dishname = {item.dishName} delivered = {item.delivered} quantity = {item.quantity} timeOfOrder = {item.time}/>
-                              )
-                         })
-                    }
-               </div>
-               <Footer lodgeId = {splitedIds[0]} roomId = {splitedIds[1]} />
-                         </div>
+                         token ? (
+                              <div>
+                                   <div className="topic text-center">
+                                        My Orders!!!
+                                   </div>
+                                   <div className="myOrders-Space">
+                                        {
+                                             data.map((item, key) => {
+                                                  return (
+                                                       <Order dishname={item.dishName} delivered={item.delivered} quantity={item.quantity} timeOfOrder={item.time} />
+                                                  )
+                                             })
+                                        }
+                                   </div>
+                                   <Footer lodgeId={splitedIds[0]} roomId={splitedIds[1]} />
+                              </div>
+                         ) : (
+                              <CustomError id={id} />
+                         )
                     )
                }
           </div>
