fc3e37c48d9d3b79f0556520d5c89b99630e3fec
Salman_JS <salman4javascript@gmail.com>
2022-11-19 00:40:06 +0530

    Minor changes

--- src/components/Variables.js
+++ src/components/Variables.js
@@ -1,12 +1,14 @@
 
 
 const Variables = {
-    host : "http://localhost:3002",
+    host : "https://salman-dev--guileless-sundae-522a29.netlify.app/.netlify/functions/server",
     dishLodge : "http://localhost:3002/632378fcf3b92aed0d106d54/",
     id : "12345678",
     login : "localhost:3000/632378fcf3b92aed0d106d54-63237959f3b92aed0d106d61/login"
 }
 
 // http://localhost:3000/634b33b6922a9eb1e046b1a3-635d9cdd7939058ccdf0f3d4/login
+
+// https://salman-dev--guileless-sundae-522a29.netlify.app/.netlify/functions/server
 
 export default Variables;
